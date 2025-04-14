ffa6913a8754e7a418c2a5e428bb5b308dd3bfcc
sushant09112004 <jadhavsushant379@gmail.com>
2025-04-14 22:18:09 +0530

    Minor chnages

--- components/app-sidebar.jsx
+++ components/app-sidebar.jsx
@@ -42,7 +42,7 @@ const data = {
       items: [
         {
           title: "Logout",
-          url: "#",
+          url: "/",
           icon: <Settings className="h-4 w-4" />,
         },
       ],
